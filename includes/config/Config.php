107fe5880e2028cd3b8443ad0a3df3e864493ef2
Josemi Xeon <worldcode.info@gmail.com>
2023-10-28 23:30:14 +0200

    mm

--- includes/config/Config.php
+++ includes/config/Config.php
@@ -2,6 +2,7 @@
 namespace MVC\includes\config;
 
 use MVC\includes\config\ConfigLocal;
+
 define('DIR_ROOT', dirname(dirname(__DIR__)));
 define('FUNCIONES_URL', DIR_ROOT.'/funciones');
 define('TEMPLATES_URL', DIR_ROOT.'/includes/templates');
