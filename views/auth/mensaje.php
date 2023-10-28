e74db05e897251957ed1a37e35bf630ad5d223c9
Josemi Xeon <worldcode.info@gmail.com>
2023-10-29 01:33:29 +0200

    Añadido mensaje de volver al inicio en la vista mensaje

--- views/auth/mensaje.php
+++ views/auth/mensaje.php
@@ -4,6 +4,8 @@
 	<div class="contenedor-sm">
 		<p class="descripcion-pagina">Hemos enviado las instrucciones para confirmar tu cuenta a tu email</p>
 
-		
+		<div class="acciones">
+			<a href="/">Iniciar Sesión</a>
+		</div>
 	</div> <!-- .contenedor-sm -->
 </div>
